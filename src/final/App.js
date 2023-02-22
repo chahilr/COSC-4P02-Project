f7176f256c388570e1c0b117de285c85381fcd59
Steven Sabharwal <46654377+stevensabharwal@users.noreply.github.com>
2023-02-22 18:27:41 -0500

    IT4P02-43: Combined styles into index.css temporarily, will be organized at a future date when organization opportunities are clearer ; Added styling for the home page exhibition buttons and changed the html structuring/wrapping of them ; Added the node_modules folder to the git ignore.

--- src/final/App.js
+++ src/final/App.js
@@ -3,19 +3,26 @@ import SelectButton from './components/SelectButton';
 function App() {
   return (
     <>
-      <ul className="exhibit-options-list">
-        <li>
-          <SelectButton name="Ancient Greece" />
-        </li>
-        <li>
-          <SelectButton name="Ancient Rome" />
-        </li>
-        <li>
-          <SelectButton name="Ancient Egypt" />
-        </li>
-        <li>
-          <SelectButton name="Persian Empire" />
-        </li>
+      <ul id="exhibit-options-list">
+        {/* First set of buttons, further apart */}
+        <div id="exhibit-options-outer">
+          <li>
+            <SelectButton name="Ancient Greece" />
+          </li>
+          <li>
+            <SelectButton name="Ancient Rome" />
+          </li>
+        </div>
+
+        {/* Second set of buttons, closer together */}
+        <div id="exhibit-options-inner">
+          <li>
+            <SelectButton name="Ancient Egypt" />
+          </li>
+          <li>
+            <SelectButton name="Persian Empire" />
+          </li>
+        </div>
       </ul>
     </>
   );
