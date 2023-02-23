6682543d7eecda9f5a20eaf3e698aa3c6cfb7728
Steven Sabharwal <46654377+stevensabharwal@users.noreply.github.com>
2023-02-23 01:21:04 -0500

    IT4P02-43: Changed the background images used so that all are 1920*1080 ; Added background image change on hover of exhibit buttons (still has no transitions... not sure how to get that working yet)

--- src/final/App.js
+++ src/final/App.js
@@ -1,11 +1,55 @@
 import SelectButton from './components/SelectButton';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
+import ancientgreece from '../images/ancient-greece.png';
+import ancientrome from '../images/ancient-rome.png';
+import ancientegypt from '../images/ancient-egypt.png';
+import persianempire from '../images/persian-empire.png';
+import museum from '../images/museum.png';
+
+
 
 function App() {
+  /** Adds the class 'home' to the body for the purpose of dynamic background image changes. */
   useEffect(() => {
     document.body.className = 'home';
     return () => { document.body.className = ''; }
   });
+
+  /** Handles mousing over any of the exhibit buttons.
+   * @TODO: Add transition to the background image change.
+   */
+  const handleMouseEnter = (event) => {
+    const target = event.target;
+    const button_id = ((target.innerText).toLowerCase()).replace(/\s/g, '');
+    let image_id;
+
+    switch(button_id) {
+      case 'ancientgreece':
+        image_id = {ancientgreece}.ancientgreece
+        break;
+
+      case 'ancientrome':
+        image_id = {ancientrome}.ancientrome
+        break;
+
+      case 'ancientegypt':
+        image_id = {ancientegypt}.ancientegypt
+        break;
+
+      case 'persianempire':
+        image_id = {persianempire}.persianempire
+        break;
+    }
+    document.body.style.background = "url(" + image_id + ")";
+  };
+
+  /** Handles mousing away from a button... changes the background image back to default
+   * @TODO: Add transition to background image change.
+   */
+  const handleMouseLeave = (event) => {
+    const image_id = {museum}.museum
+    document.body.style.background = "url(" + image_id + ")";
+  };
 
   return (
     <>
@@ -29,9 +73,9 @@ function App() {
         <div className='logo-text'>
           <span>Language Select</span>
         </div>
-
       </div>
 
+      {/* Create Personalized Timeline and Search for Artifact */}
       <div className="center">
         <ul id="main-button-group">
           <li>
@@ -43,25 +87,26 @@ function App() {
         </ul>
       </div>
 
+      {/* Travel Through Time Banner */}
       <span className="translucent-banner">Travel Through Time</span>
 
       <ul id="exhibit-options-list">
         {/* First set of buttons, further apart */}
-        <div id="exhibit-options-outer">
-          <li>
-            <SelectButton name="Ancient Greece" />
+        <div id="exhibit-options-r1">
+          <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <SelectButton name="Ancient Greece"  />
           </li>
-          <li>
+          <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
             <SelectButton name="Ancient Rome" />
           </li>
         </div>
 
         {/* Second set of buttons, closer together */}
-        <div id="exhibit-options-inner">
-          <li>
+        <div id="exhibit-options-r2">
+          <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
             <SelectButton name="Ancient Egypt" />
           </li>
-          <li>
+          <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
             <SelectButton name="Persian Empire" />
           </li>
         </div>
