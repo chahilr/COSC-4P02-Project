669c54ada6a37e427ad7b5226b28afd72a72d0a5
Reet Chahil <chahilr@outlook.com>
2023-03-12 15:03:42 -0400

    Added Timeline page and Timeline css file. Fixed Artifact Overview style bug

--- src/final/App.js
+++ src/final/App.js
@@ -3,14 +3,15 @@ import ArtifactOverview from './pages/ArtifactOverview';
 
 import Home from './pages/Home';
 import TimelineCustomizer from './pages/TimelineCustomizer';
+import Timeline from './pages/Timeline';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/customizer" element={<TimelineCustomizer />} />
-      <Route path="/artifact" element={<ArtifactOverview/>} />
-      
+      <Route path="/artifact" element={<ArtifactOverview />} />
+      <Route path="/timeline" element={<Timeline />} />
     </Routes>
   );
 }
