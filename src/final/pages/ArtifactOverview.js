2f4e4cd2e9490310ea52aaca9ca50b03325fb1e0
Steven Sabharwal <46654377+stevensabharwal@users.noreply.github.com>
2023-03-31 02:23:59 -0400

    Changed layout for /artifact mobileview. Made changes to the styling of the navigations buttons on the bottom of the page.

--- src/final/pages/ArtifactOverview.js
+++ src/final/pages/ArtifactOverview.js
@@ -17,7 +17,15 @@ export default function ArtifactOverview() {
   );
   return (
     <>
-      <Logo color="var(--white)" />
+      <Logo
+        color="var(--white)"
+        style={{
+          position: 'relative',
+          marginBottom: 0,
+          marginTop: 0,
+          backgroundColor: 'black',
+        }}
+      />
       <div className={styles['container']}>
         <div className={styles['artifact-container']}>
           <div className={styles['artifact-text']}>
@@ -28,10 +36,12 @@ export default function ArtifactOverview() {
             <img src={state.image} alt="" height={'100dvh'} />
           </div>
         </div>
-        <div className={styles['button-container']}>
-          <button id="return-button">Return</button>
-          <button id="related-artifact-button">Related Artifact</button>
-          <button id="other-media-button">Other Media</button>
+        <div className={styles['nav-container']}>
+          <div className={styles['button-container']}>
+            <button id="return-button">Back to Timeline</button>
+            <button id="related-artifact-button">Related Artifact</button>
+            <button id="other-media-button">Other Media</button>
+          </div>
         </div>
       </div>
     </>
