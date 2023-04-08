f7c495a855f13fe5f156af2d5bc9f466af8ec2b3
Fahad Umair Durrani <ifahad31@gmail.com>
2023-04-07 23:22:45 -0400

    added picture functionality and tags

--- src/utils/FirebaseApp.js
+++ src/utils/FirebaseApp.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 //The Firebase configuration
 const app = initializeApp({
@@ -14,4 +15,5 @@ const app = initializeApp({
 });
 
 export const firestore = getFirestore(app);
+export const storage = getStorage(app);
 export const auth = getAuth(app);
