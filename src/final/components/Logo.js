d1aa433c8535777d3aa61d7473ba61295f711776
Henry Tu <ht17dk@brocku.ca>
2023-04-08 19:21:44 -0400

    Made the logo link to the home page

--- src/final/components/Logo.js
+++ src/final/components/Logo.js
@@ -1,4 +1,5 @@
 import styles from '../styles/Logo.module.css';
+import { Link } from 'react-router-dom';
 
 export default function Logo(props) {
   return (
@@ -9,25 +10,31 @@ export default function Logo(props) {
       }}
     >
       {/* Logo */}
-      <div className={styles['logo']}>
-        <div className={styles['logo-image']}>
-          <img
-            src={require('../../images/museum-logo.jpg')}
-            alt="Museum logo"
-            style={{ float: 'left', marginRight: '10px' }}
-          />
-        </div>
+      <Link to="/"style={{ textDecoration: 'none' }}>
+        <div className={styles['logo']}>
+          <div className={styles['logo-image']}>
+            <img
+              src={require('../../images/museum-logo.jpg')}
+              alt="Museum logo"
+              style={{ float: 'left', marginRight: '10px' }}
+            />
+          </div>
 
-        <div className={styles['logo-text']}>
-          <span style={{ color: props.color }}>Niagara On The Lake</span>
-          <hr style={{ borderColor: props.color }}></hr>
-          <span style={{ color: props.color }}>
-            History Museum
-          </span>
-        </div>
+          <div className={styles['logo-text']}>
+            <span style={{ color: props.color }}>
 
-        <div style={{ clear: 'both' }}></div>
-      </div>
+              Niagara On The Lake
+
+            </span>
+            <hr style={{ borderColor: props.color }}></hr>
+            <span style={{ color: props.color }}>
+              History Museum
+            </span>
+          </div>
+
+          <div style={{ clear: 'both' }}></div>
+        </div>
+      </Link>
     </div>
   );
 }
