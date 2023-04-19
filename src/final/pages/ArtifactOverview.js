137ba28ead1b76defcd30421b34b60f2c9615084
arshdeep42 <arshdeep42singh@gmail.com>
2023-04-19 14:53:50 -0400

    css and js for the artifact overview page

--- src/final/pages/ArtifactOverview.js
+++ src/final/pages/ArtifactOverview.js
@@ -19,23 +19,21 @@ export default function ArtifactOverview() {
     <>
       <Logo
         color="var(--white)"
-        style={{position: 'relative', backgroundColor: "var(--translucent-grey)", marginTop: "unset", padding: "1dvh 1em"}}
+        style={{ position: 'relative', backgroundColor: "var(--translucent-grey)", marginTop: "0", padding: "1em", borderRadius: "5px" }}
       />
       <div className={styles['container']}>
-        <div className={styles['artifact-container']}>
-          <div className={styles['artifact-text']}>
-            <h2>{state.title}</h2>
-            <p>{state.description ?? description}</p>
-          </div>
-          <div className={styles['artifact-image']}>
-            <img src={state.image} alt="" height={'100dvh'} />
-          </div>
+        <div className={styles['artifact-text']} style={{ position: 'absolute', top: '20%', left: '5%', zIndex: 10 }}>
+          <h2 style={{ fontSize: '2.5rem', marginBottom: '1em' }}>{state.title}</h2>
+          <p style={{ fontSize: '1.4rem', lineHeight: '1.6', textAlign: 'justify' }}>{state.description ?? description}</p>
         </div>
-        <div className={styles['nav-container']}>
+        <div className={styles['artifact-image']} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%' }}>
+          <img src={state.image} alt="" style={{ maxWidth: '95%', maxHeight: '90vh', objectFit: 'contain', borderRadius: '5px', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)' }} />
+        </div>
+        <div className={styles['nav-container']} style={{ marginTop: '2em' }}>
           <div className={styles['button-container']}>
-            <button id="return-button">Back to Timeline</button>
-            <button id="related-artifact-button">Related Artifact</button>
-            <button id="other-media-button">Other Media</button>
+            <button id="return-button" style={{ fontSize: '1.2rem', padding: '12px 24px', borderRadius: '5px', marginRight: '1em' }}>Back to Timeline</button>
+            <button id="related-artifact-button" style={{ fontSize: '1.2rem', padding: '12px 24px', borderRadius: '5px', marginRight: '1em' }}>Related Artifact</button>
+            <button id="other-media-button" style={{ fontSize: '1.2rem', padding: '12px 24px', borderRadius: '5px' }}>Other Media</button>
           </div>
         </div>
       </div>
