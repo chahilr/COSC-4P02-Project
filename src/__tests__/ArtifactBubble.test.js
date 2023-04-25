f8805e3ce8c269c5d23e28fe7de7faea668f51d9
arshdeep42 <arshdeep42singh@gmail.com>
2023-04-25 19:45:36 -0400

    Artifact Bubble Test Passed

--- src/__tests__/ArtifactBubble.test.js
+++ src/__tests__/ArtifactBubble.test.js
@@ -1,10 +1,10 @@
+// ArtifactBubble.test.js
+
 import React from 'react';
 import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
-import { BrowserRouter } from 'react-router-dom'
-import ArtifactBubble from '../components/ArtifactBubble.js';
-import styles from '../styles/ArtifactBubble.module.css';
-
+import { BrowserRouter } from 'react-router-dom';
+import ArtifactBubble from '../components/ArtifactBubble';
 
 let container = null;
 beforeEach(() => {
@@ -19,17 +19,30 @@ afterEach(() => {
   container = null;
 });
 
-  test('ArtifactBubble component', () => {
-  	act(() => {
-  	render(<BrowserRouter><ArtifactBubble
-                    visible={true}
-                    artifact={Exhibition='Ancient Greece'}
-                    alternator={null}
-                    props=
-                  /></BrowserRouter>, container);
-  	
-  	});
-  expect(container.textContent).toContain("e");
-    //render(<App/>);
-    //expect(screen.getByText('e')).toBeInTheDocument();
+test('ArtifactBubble component', () => {
+  const sampleArtifact = {
+    Exhibition: 'Ancient Greece',
+    Name: 'Sample Artifact',
+    Year: -500,
+    Photos: ['sample-image-url.jpg'],
+    // Add other properties required by ArtifactBubble component
+  };
+
+  act(() => {
+    render(
+      <BrowserRouter>
+        <ArtifactBubble
+          visible={true}
+          artifact={sampleArtifact}
+          alternator={0} // Set the alternator to 0 so that the first condition is met
+        />
+      </BrowserRouter>,
+      container
+    );
   });
+
+  const imageElement = container.querySelector('img');
+  expect(imageElement.style.border).toContain('green');
+  expect(container.textContent).toContain("Sample Artifact");
+  expect(container.textContent).toContain("500 BC");
+});
