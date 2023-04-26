0fd34fc52c7c72ee336cd83bcace382b87125f80
arshdeep42 <arshdeep42singh@gmail.com>
2023-04-25 20:15:25 -0400

    selectbutton test passed

--- src/__tests__/hello.test.js
+++ src/__tests__/hello.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
-import Hello from './hello.js'
+import Hello from '../components/hello.js'
 
 let container = null;
 beforeEach(() => {
